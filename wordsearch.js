3dbb60a2b43e630a1f8f704939a40d2e1154edfe
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-03 17:07:14 -0400

    implemented a word searcher

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  let newArray = [];
+  for (let i = 0;  i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
 
-module.exports = wordSearch
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      newArray[c][r] = matrix[r][c];
+    }
+  }
+  return newArray;
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+//wordSearch ("entire array", "frank")
+  
+
+
+module.exports = wordSearch;
